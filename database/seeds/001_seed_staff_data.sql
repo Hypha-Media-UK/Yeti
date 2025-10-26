f54c9147b99102f14af6e97de56e79b4f21badb4
Hypha Media <113041015+Hypha-Media-UK@users.noreply.github.com>
2025-10-26 11:04:45 +0000

    Update staff seed data with actual staff members
    - 60 Regular Day staff (distributed across 4 groups with offsets 0, 2, 4, 6)
    - 12 Regular Night staff (distributed across 3 groups with offsets 0, 2, 4)
    - 4 Supervisors (with offsets 0, 4, 8, 12 for 16-day cycle coverage)
    - 7 Relief staff (manually assigned, no cycle)
    
    Total: 83 staff members
    
    Staff names provided by user, organized by role and shift pattern

--- database/seeds/001_seed_staff_data.sql
+++ database/seeds/001_seed_staff_data.sql
@@ -1,47 +1,113 @@
--- Seed staff data with diverse roles, groups, and offsets for testing
+-- Seed staff data with actual staff members
+-- Distribution: ~60 Regular Day, ~20 Regular Night, ~5 Supervisors, ~5 Relief
 
--- Regular Day Staff (4-on-4-off pattern)
+-- Regular Day Staff - Group A (offset 0)
 INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
-('James', 'Smith', 'Regular', 'Day', '4-on-4-off', 0),
-('Mary', 'Johnson', 'Regular', 'Day', '4-on-4-off', 0),
-('Robert', 'Williams', 'Regular', 'Day', '4-on-4-off', 0),
-('Patricia', 'Brown', 'Regular', 'Day', '4-on-4-off', 0),
-('Michael', 'Jones', 'Regular', 'Day', '4-on-4-off', 4),
-('Jennifer', 'Garcia', 'Regular', 'Day', '4-on-4-off', 4),
-('William', 'Miller', 'Regular', 'Day', '4-on-4-off', 4),
-('Linda', 'Davis', 'Regular', 'Day', '4-on-4-off', 4);
+('Alan', 'Clark', 'Regular', 'Day', '4-on-4-off', 0),
+('Alan', 'Kelly', 'Regular', 'Day', '4-on-4-off', 0),
+('Andrew', 'Gibson', 'Regular', 'Day', '4-on-4-off', 0),
+('Andrew', 'Hassall', 'Regular', 'Day', '4-on-4-off', 0),
+('Andrew', 'Trudgeon', 'Regular', 'Day', '4-on-4-off', 0),
+('Brian', 'Cassidy', 'Regular', 'Day', '4-on-4-off', 0),
+('Chris', 'Huckaby', 'Regular', 'Day', '4-on-4-off', 0),
+('Chris', 'Roach', 'Regular', 'Day', '4-on-4-off', 0),
+('Chris', 'Wardle', 'Regular', 'Day', '4-on-4-off', 0),
+('Colin', 'Bromley', 'Regular', 'Day', '4-on-4-off', 0),
+('Craig', 'Butler', 'Regular', 'Day', '4-on-4-off', 0),
+('Darren', 'Flowers', 'Regular', 'Day', '4-on-4-off', 0),
+('Darren', 'Milhench', 'Regular', 'Day', '4-on-4-off', 0),
+('Darren', 'Mycroft', 'Regular', 'Day', '4-on-4-off', 0),
+('David', 'Sykes', 'Regular', 'Day', '4-on-4-off', 0);
 
--- Regular Night Staff (4-on-4-off pattern)
+-- Regular Day Staff - Group B (offset 4)
 INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
-('David', 'Rodriguez', 'Regular', 'Night', '4-on-4-off', 0),
-('Barbara', 'Martinez', 'Regular', 'Night', '4-on-4-off', 0),
-('Richard', 'Hernandez', 'Regular', 'Night', '4-on-4-off', 0),
-('Susan', 'Lopez', 'Regular', 'Night', '4-on-4-off', 0),
-('Joseph', 'Gonzalez', 'Regular', 'Night', '4-on-4-off', 4),
-('Jessica', 'Wilson', 'Regular', 'Night', '4-on-4-off', 4),
-('Thomas', 'Anderson', 'Regular', 'Night', '4-on-4-off', 4),
-('Sarah', 'Thomas', 'Regular', 'Night', '4-on-4-off', 4);
+('Dean', 'Pickering', 'Regular', 'Day', '4-on-4-off', 4),
+('Duane', 'Kulikowski', 'Regular', 'Day', '4-on-4-off', 4),
+('Edward', 'Collier', 'Regular', 'Day', '4-on-4-off', 4),
+('Gary', 'Booth', 'Regular', 'Day', '4-on-4-off', 4),
+('Gary', 'Bromley', 'Regular', 'Day', '4-on-4-off', 4),
+('Gavin', 'Marsden', 'Regular', 'Day', '4-on-4-off', 4),
+('Ian', 'Moss', 'Regular', 'Day', '4-on-4-off', 4),
+('Ian', 'Speakes', 'Regular', 'Day', '4-on-4-off', 4),
+('Jake', 'Moran', 'Regular', 'Day', '4-on-4-off', 4),
+('James', 'Bennett', 'Regular', 'Day', '4-on-4-off', 4),
+('James', 'Mitchell', 'Regular', 'Day', '4-on-4-off', 4),
+('Jason', 'Newton', 'Regular', 'Day', '4-on-4-off', 4),
+('Jeff', 'Robinson', 'Regular', 'Day', '4-on-4-off', 4),
+('Joe', 'Redfearn', 'Regular', 'Day', '4-on-4-off', 4),
+('John', 'Evans', 'Regular', 'Day', '4-on-4-off', 4);
+
+-- Regular Day Staff - Group C (offset 2)
+INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
+('Jordon', 'Fish', 'Regular', 'Day', '4-on-4-off', 2),
+('Kevin', 'Gaskell', 'Regular', 'Day', '4-on-4-off', 2),
+('Kevin', 'Tomlinson', 'Regular', 'Day', '4-on-4-off', 2),
+('Kyle', 'Blackshaw', 'Regular', 'Day', '4-on-4-off', 2),
+('Kyle', 'Sanderson', 'Regular', 'Day', '4-on-4-off', 2),
+('Lee', 'Stafford', 'Regular', 'Day', '4-on-4-off', 2),
+('Lewis', 'Yearsley', 'Regular', 'Day', '4-on-4-off', 2),
+('Mark', 'Dickinson', 'Regular', 'Day', '4-on-4-off', 2),
+('Mark', 'Haughton', 'Regular', 'Day', '4-on-4-off', 2),
+('Mark', 'Lloyd', 'Regular', 'Day', '4-on-4-off', 2),
+('Mark', 'Walton', 'Regular', 'Day', '4-on-4-off', 2),
+('Martin', 'Hobson', 'Regular', 'Day', '4-on-4-off', 2),
+('Martin', 'Kenyon', 'Regular', 'Day', '4-on-4-off', 2),
+('Matthew', 'Bennett', 'Regular', 'Day', '4-on-4-off', 2),
+('Matthew', 'Cope', 'Regular', 'Day', '4-on-4-off', 2);
+
+-- Regular Day Staff - Group D (offset 6)
+INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
+('Matthew', 'Rushton', 'Regular', 'Day', '4-on-4-off', 6),
+('Merv', 'Permalloo', 'Regular', 'Day', '4-on-4-off', 6),
+('Michael', 'Shaw', 'Regular', 'Day', '4-on-4-off', 6),
+('Mike', 'Brennan', 'Regular', 'Day', '4-on-4-off', 6),
+('Nigel', 'Beesley', 'Regular', 'Day', '4-on-4-off', 6),
+('Paul', 'Berry', 'Regular', 'Day', '4-on-4-off', 6),
+('Paul', 'Fisher', 'Regular', 'Day', '4-on-4-off', 6),
+('Paul', 'Flowers', 'Regular', 'Day', '4-on-4-off', 6),
+('Peter', 'Moss', 'Regular', 'Day', '4-on-4-off', 6),
+('Phil', 'Hollinshead', 'Regular', 'Day', '4-on-4-off', 6),
+('Regan', 'Stringer', 'Regular', 'Day', '4-on-4-off', 6),
+('Rob', 'Mcpartland', 'Regular', 'Day', '4-on-4-off', 6),
+('Robert', 'Frost', 'Regular', 'Day', '4-on-4-off', 6),
+('Roy', 'Harris', 'Regular', 'Day', '4-on-4-off', 6),
+('Scott', 'Cartledge', 'Regular', 'Day', '4-on-4-off', 6);
+
+-- Regular Night Staff - Group A (offset 0)
+INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
+('Simon', 'Collins', 'Regular', 'Night', '4-on-4-off', 0),
+('Soloman', 'Offei', 'Regular', 'Night', '4-on-4-off', 0),
+('Stepen', 'Bowater', 'Regular', 'Night', '4-on-4-off', 0),
+('Stephen', 'Burke', 'Regular', 'Night', '4-on-4-off', 0),
+('Stephen', 'Cooper', 'Regular', 'Night', '4-on-4-off', 0);
+
+-- Regular Night Staff - Group B (offset 4)
+INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
+('Stephen', 'Kirk', 'Regular', 'Night', '4-on-4-off', 4),
+('Stephen', 'Scarsbrook', 'Regular', 'Night', '4-on-4-off', 4),
+('Steven', 'Richardson', 'Regular', 'Night', '4-on-4-off', 4),
+('Stuart', 'Ford', 'Regular', 'Night', '4-on-4-off', 4),
+('Stuart', 'Lomas', 'Regular', 'Night', '4-on-4-off', 4);
+
+-- Regular Night Staff - Group C (offset 2)
+INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
+('Tomas', 'Konkol', 'Regular', 'Night', '4-on-4-off', 2),
+('Tony', 'Batters', 'Regular', 'Night', '4-on-4-off', 2);
 
 -- Supervisors (4 days / 4 off / 4 nights / 4 off pattern)
 INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
-('Charles', 'Taylor', 'Supervisor', NULL, 'supervisor', 0),
-('Karen', 'Moore', 'Supervisor', NULL, 'supervisor', 0),
-('Christopher', 'Jackson', 'Supervisor', NULL, 'supervisor', 8),
-('Nancy', 'Martin', 'Supervisor', NULL, 'supervisor', 8);
+('Allen', 'Butler', 'Supervisor', NULL, 'supervisor', 0),
+('Andy', 'Clayton', 'Supervisor', NULL, 'supervisor', 4),
+('Carla', 'Barton', 'Supervisor', NULL, 'supervisor', 8),
+('Charlotte', 'Rimmer', 'Supervisor', NULL, 'supervisor', 12);
 
 -- Relief Staff (manually assigned)
 INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
-('Daniel', 'Lee', 'Relief', NULL, NULL, 0),
-('Betty', 'Perez', 'Relief', NULL, NULL, 0),
-('Matthew', 'White', 'Relief', NULL, NULL, 0),
-('Dorothy', 'Harris', 'Relief', NULL, NULL, 0);
-
--- Additional Regular Staff with different offsets for variety
-INSERT INTO staff (first_name, last_name, status, `group`, cycle_type, days_offset) VALUES
-('Mark', 'Sanchez', 'Regular', 'Day', '4-on-4-off', 2),
-('Lisa', 'Clark', 'Regular', 'Day', '4-on-4-off', 6),
-('Donald', 'Ramirez', 'Regular', 'Night', '4-on-4-off', 2),
-('Sandra', 'Lewis', 'Regular', 'Night', '4-on-4-off', 6),
-('Steven', 'Robinson', 'Regular', 'Day', '4-on-4-off', -2),
-('Ashley', 'Walker', 'Regular', 'Night', '4-on-4-off', -2);
+('AJ', '', 'Relief', NULL, NULL, 0),
+('Eloisa', 'Andrew', 'Relief', NULL, NULL, 0),
+('Julie', 'Greenough', 'Relief', NULL, NULL, 0),
+('Karen', 'Blackett', 'Relief', NULL, NULL, 0),
+('Lucy', 'Redfearn', 'Relief', NULL, NULL, 0),
+('Lynne', 'Warner', 'Relief', NULL, NULL, 0),
+('Nicola', 'Benger', 'Relief', NULL, NULL, 0);
 
